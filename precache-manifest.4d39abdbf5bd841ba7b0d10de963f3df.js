442b9ed04d741785f2e99d6886012614c1772966
AxelHenriot <axlht@hotmail.com>
2019-04-27 11:19:25 -0300

    Updates

---
+++ precache-manifest.4d39abdbf5bd841ba7b0d10de963f3df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cace32c298a151e54b94",
+    "url": "/contactmanagerContext/static/js/runtime~main.8f9d3c51.js"
+  },
+  {
+    "revision": "ea166d890acea378c64f",
+    "url": "/contactmanagerContext/static/js/main.24f69dca.chunk.js"
+  },
+  {
+    "revision": "663237b115e035e20b08",
+    "url": "/contactmanagerContext/static/js/2.20d82ee2.chunk.js"
+  },
+  {
+    "revision": "ea166d890acea378c64f",
+    "url": "/contactmanagerContext/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "663237b115e035e20b08",
+    "url": "/contactmanagerContext/static/css/2.ae1d9e27.chunk.css"
+  },
+  {
+    "revision": "2146237842822cc4b8659e6a3ed2d10a",
+    "url": "/contactmanagerContext/index.html"
+  }
+];
